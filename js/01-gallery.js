21e5be584d85f2f69511a65cffa8748ea47cbc85
SyroidNadiia <112964918+SyroidNadiia@users.noreply.github.com>
2023-02-07 18:00:55 +0200

    task1.1

---
+++ js/01-gallery.js
@@ -0,0 +1,34 @@
+import { galleryItems } from './gallery-items.js';
+// Change code below this line
+
+const galleryRef = document.querySelector('.gallery');
+const elementGalleryMakeup = createElementGallery(galleryItems);
+
+console.log(createElementGallery(galleryItems));
+galleryRef.insertAdjacentHTML('beforeend', elementGalleryMakeup);
+
+
+function createElementGallery(galleryItems) {
+    return galleryItems.map(({preview, original, description}) => {
+        return `
+        <div class="gallery__item">
+          <a class="gallery__link" href="${original}">
+            <img
+              class="gallery__image"
+              src="${preview}"
+              data-source="${original}"
+              alt="${description}"
+            />
+          </a>
+        </div>
+        `
+    })
+    .join('');
+
+// console.log(markup[1]);
+}
+
+// console.log(galleryItems);
+// 
+
+
